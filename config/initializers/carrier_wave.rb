c11b602be808838b96d6894f071390a55ef17edb
Alexey Lukyanov <alukyanov@groupon.com>
2015-01-14 17:12:50 +0700

    fix amazon region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || 'xxx',     # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || 'xxx', # required
-    #:region                 => 'Ireland',                  # optional, defaults to 'us-east-1'
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
     #:host                   => 's3-eu-west-1.amazonaws.com',             # optional, defaults to nil
     #:endpoint               => 'http://paupers.s3-eu-west-1.amazonaws.com' # optional, defaults to nil,
   }
