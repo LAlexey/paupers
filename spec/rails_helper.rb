ab005c824cebd12f4f6d70d7832ca5e211aaa9f1
Alexey Lukyanov <alukyanov.pro@gmail.com>
2016-03-27 02:02:55 +0700

    xasxsa

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 60, phantomjs: '/usr/bin')
+  Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 60, phantomjs: '/usr/share/phantomjs/bin/phantomjs')
 end
 Capybara.javascript_driver = :poltergeist
 
